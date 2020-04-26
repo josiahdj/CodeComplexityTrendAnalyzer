866b5b723eba950dc219789e40d81eb05a221886
Josiah Johnson <josiah.johnson@olo.com>
2020-04-26 11:42:07 -0700

    Save CommitInfo to DB

--- CodeComplexityTrendAnalyzer/Database.fs
+++ CodeComplexityTrendAnalyzer/Database.fs
@@ -1,12 +1,15 @@
 ﻿namespace CodeComplexityTrendAnalyzer
 
 open FSharp.Data.Sql
+open System.Data.SqlClient
+open System
 
 module Database = 
     let [<Literal>] dbVendor = Common.DatabaseProviderTypes.MSSQLSERVER
-    let [<Literal>] connString = "Data Source=.;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False"
+    let [<Literal>] connString = "Server=localhost;Database=CodeAnalysis;Integrated Security=True;"
     let [<Literal>] indivAmount = 1000
     let [<Literal>] useOptTypes  = true
+    let [<Literal>] contextSchemaPath = __SOURCE_DIRECTORY__ + @".\Schema.json" // ,ContextSchemaPath = contextSchemaPath
     
     type sql =
         SqlDataProvider<
@@ -16,10 +19,21 @@ module Database =
             UseOptionTypes = useOptTypes>
     
     let ctx = sql.GetDataContext()
+    // ctx.SaveContextSchema() |> ignore
+    let commitInfo = ctx.Dbo.CommitInfo
+
 
     let toTable data =
         ()
 
     let saveCommitInfo (ci : CommitInfo) =
-        //let commitInfo = ctx.Dbo.CommitInfo
+        let (success, date) = DateTime.TryParse(ci.Date)
+        if success then
+            try 
+                let newCi = commitInfo.``Create(Author, Date, Hash)``(ci.Author, date, ci.Hash)
+                ctx.SubmitUpdates()
+                logger.Debug("Created commit record with hash {Hash} and Id {Id}", newCi.Hash, newCi.Id)
+            with 
+            | :? SqlException as ex when ex.Message.Contains("unique") -> 
+                logger.Debug("The commit with hash {Hash} already exists.", ci.Hash)
         ()
--- Database/Tables/CommitInfo.sql
+++ Database/Tables/CommitInfo.sql
@@ -3,6 +3,9 @@
     [Hash]   NVARCHAR (255) NOT NULL,
     [Date]   DATETIME2 (7)  NOT NULL,
     [Author] NVARCHAR (255) NOT NULL,
-    CONSTRAINT [PK_CommitInfo] PRIMARY KEY CLUSTERED ([Id] ASC)
+    CONSTRAINT [PK_CommitInfo] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [IX_UNIQUE_CommitInfo_Hash] UNIQUE([Hash])   
 );
+GO
 
+CREATE INDEX [IX_CommitInfo_Fields] ON [dbo].[CommitInfo] ([Hash],[Date],[Author])
